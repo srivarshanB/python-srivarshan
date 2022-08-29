fb0064086c0187f6bb205eb493ca9c20ee9d7786
srivarshanB <112372900+srivarshanB@users.noreply.github.com>
2022-08-29 14:00:28 +0530

    Update quadratic equation.py

--- quadratic equation.py
+++ quadratic equation.py
@@ -1,8 +1,8 @@
-import cmath
-a=1
-b=5
-c=6
+import math
+a=int(input("enter the number1:"))
+b=int(input("enter the number2:"))
+c=int(input("enter the number3:"))
 d=(b**2)-(4*a*c)
-ans1=(-b-cmath.sqrt(d))/(2*a)
-ans2=(-b+cmath.sqrt(d))/(2*a)
+ans1=(-b-math.sqrt(d))/(2*a)
+ans2=(-b+math.sqrt(d))/(2*a)
 print('answers are',ans1,'and',ans2)
