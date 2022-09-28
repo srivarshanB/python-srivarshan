f9e403b957fd8ff7409074ff52e8f703668ae12d
srivarshanB <112372900+srivarshanB@users.noreply.github.com>
2022-09-28 11:59:21 +0530

    Add files via upload

---
+++ rate of interest.py
@@ -0,0 +1,7 @@
+a=int(input("enter the principal amount:"))
+b=int(input("enter the number of year:"))
+c=int(input("enter the rate of interest:"))
+d=input("Is customer senior sitizen:(y/n):")
+simple_interest=a*b*c/100
+print("simple interest:",simple_interest)
+        
